9189de2284848d3cf5591351c72ba88ee159999c
OseenM <oseenmarcelino.6@gmail.com>
2025-04-03 20:28:24 -0400

    Add description

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -19,4 +19,5 @@ resource "azurerm_subnet" "subnet" {
 
 output "resource_group_name" {
   value = azurerm_resource_group.app_rg.name
+  description = "The name of the resource group where the resources are deployed."
 }
