0fb9376d4c73455b944b3d9faf6987d8ceeda684
OseenM <oseenmarcelino.6@gmail.com>
2025-04-03 20:32:58 -0400

    Move output

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -16,8 +16,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-output "resource_group_name" {
-  value = azurerm_resource_group.app_rg.name
-  description = "The name of the resource group where the resources are deployed."
-}
